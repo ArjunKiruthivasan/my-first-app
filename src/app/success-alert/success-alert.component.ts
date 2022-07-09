ec68aeab857c57a2aa5d243cd3141fa46390a590
ArjunKiruthivasan <arjun.kiruthivasan@gmail.com>
2022-07-09 12:37:31 +0530

    Border Added

--- src/app/success-alert/success-alert.component.ts
+++ src/app/success-alert/success-alert.component.ts
@@ -3,8 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-success-alert',
   templateUrl: './success-alert.component.html',
-  //styleUrls: ['./app.component.css']
-  styles: ['p { color: green; }']
+  styleUrls: ['./success-alert.component.css']
 })
 export class SuccessAlertComponent implements OnInit {
 
